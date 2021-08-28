2ea3ea77e2dfa876fa09638da10896ba799d8a49
Two-Weeks <webmon@kakao.com>
2021-08-29 01:56:36 +0900

    (3) 어떻게/어떡해 구분 추가

--- Src/spellcheck/regExp.py
+++ Src/spellcheck/regExp.py
@@ -2,16 +2,23 @@ import re
 import csv
 import os
 
-'''
-if re.findall(r"되(?=[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9])$|되$", msg):
-    msg = re.sub('되(?=[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]$)|되$',"돼",msg)
-    flag = True
-'''
-
-patterns = [
-    "되(?=[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]$)|되$"
+regs = [
+    '(?<=[^(지)]\s)않',
+    '되(?=[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]$)|되$',
+    '어떻게(?=[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]*$)',
+    '어떡해(?=[\s]*[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|]+)'
 ]
-regs = []
+
+corrects = [
+    '안',
+    '돼',
+    '어떡해',
+    '어떻게'
+]
+
+patterns = []
+
+legnth = len(corrects)
 
 def init():
     # 1단계 init
@@ -25,9 +32,8 @@ def init():
     global fileName
     fileName = dataPath + '\data1.csv'
 
-    for pattern in patterns:
-        regs.append(re.compile(patterns))
-    
+    for reg in regs:
+        patterns.append(re.compile(reg))
 
 
 def check(msg):
@@ -35,18 +41,24 @@ def check(msg):
     flag = False
 
     # 1단계 csv 파일 패턴 체크
-    matchFlag = False
+    csvFlag = False
     # 파일 읽기
     with open(fileName, 'r', encoding='utf-8') as f:
         data = csv.reader(f)       
         for line in data:
             if re.findall(line[0], msg):
                 msg = msg.replace(line[0], line[1])
-                matchFlag = True
+                csvFlag = True
 
 
     # 2단계 유형 체크
+    patternFlag = False
+    length = len(patterns)
+    for i in range(length):
+        if re.search(patterns[i], msg):
+            msg = re.sub(patterns[i], corrects[i], msg)
+            patternFlag = True
 
-
+    flag = csvFlag or patternFlag
 
     return msg, flag
